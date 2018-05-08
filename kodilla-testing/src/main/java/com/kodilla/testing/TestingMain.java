2ec93a79ca1a579fea716c4ee8a7a039bdd47712
Staszewski Damian <staszewsd@gmail.com>
2018-05-08 19:12:29 +0100

    Added 2 classes(SimpleUser, Calculator) and made testing on them.

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // test klasy kalkulator
+
+        int a = 2;
+        int b = 4;
+
+        if (Calculator.Add(a, b) == 6 && Calculator.Subtract(b, a) == 2) {
+            System.out.println("test klasy calculator OK");
+        } else {
+            System.out.println("Error w klasie calculator!");
+        }
+
     }
 }
