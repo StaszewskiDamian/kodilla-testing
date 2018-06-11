aed3b56be9a8d61691424ea71fb22454d42ab423
Staszewski Damian <staszewsd@gmail.com>
2018-06-11 18:07:55 +0100

    Added changes to RPS game.

---
+++ kodilla-rps/src/main/java/com/kodilla/rps/game/GameLogic.java
@@ -0,0 +1,55 @@
+package com.kodilla.rps.game;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class GameLogic {
+
+    public GameLogic runGame() {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.rps");
+        CpuMove cpuChoice = context.getBean(CpuMove.class);
+        UserMove userMove = context.getBean(UserMove.class);
+        ScoreValidator scoreValidator = context.getBean(ScoreValidator.class);
+
+        userMove.beginGame();
+        loop:
+        while (true) {
+            Choice user = userMove.playGame();
+            Choice cpu = cpuChoice.getCpuMove();
+
+            int compareMoves = scoreValidator.compareMoves(cpu, user);
+
+            if (compareMoves == 0) {
+                userMove.gamePlayed++;
+                System.out.println("Tie. Both player chose: " + user);
+                System.out.println("Score: cpu = " + userMove.getCpuScore() + " " + userMove.getUserName() + " = " + userMove.getUserScore());
+            } else if (compareMoves == 2) {
+                System.out.println("You leave the game.");
+                break loop;
+            } else if (compareMoves == 1) {
+                userMove.cpuScore++;
+                userMove.gamePlayed++;
+                System.out.println(cpu + " beats " + user + ", " + userMove.getUserName() + " loose the round.");
+                System.out.println("Score: cpu = " + userMove.getCpuScore() + " " + userMove.getUserName() + " = " + userMove.getUserScore());
+            } else if (compareMoves == -1) {
+                userMove.userScore++;
+                userMove.gamePlayed++;
+                System.out.println(user + " beats " + cpu + ", " + userMove.getUserName() + " win the round.");
+                System.out.println("Score: cpu = " + userMove.getCpuScore() + " " + userMove.getUserName() + " = " + userMove.getUserScore());
+            }
+            if (userMove.getCpuScore() == userMove.getWinStreak() || userMove.getUserScore() == userMove.getWinStreak()) {
+                System.out.println("Game over.");
+                System.out.println("Game played: " + userMove.getGamePlayed());
+                if (userMove.getCpuScore() > userMove.getUserScore()) {
+                    System.out.println(userMove.getUserName() + " looses " + userMove.getUserScore() + " to " + userMove.getCpuScore());
+                } else {
+                    System.out.println(userMove.getUserName() + " wins " + userMove.getUserScore() + " to " + userMove.getCpuScore());
+                }
+                break;
+            }
+        }
+    return new GameLogic();
+    }
+}
+
