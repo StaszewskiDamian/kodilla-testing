8807d1d5daa50ff584a8697df33c2d9192728739
Staszewski Damian <staszewsd@gmail.com>
2018-09-14 20:19:16 +0100

    Added module 23 of Kodilla Bootcamp: Debugging, Selenium, XPath

--- kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
@@ -1,7 +1,8 @@
 package com.kodilla.stream.invoice.simple;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class SimpleInvoiceTestSuite {
     @Test
@@ -11,8 +12,9 @@ public class SimpleInvoiceTestSuite {
         //When
         invoice.addItem(new SimpleItem(new SimpleProduct("Product 1", 17.28), 2.0));
         invoice.addItem(new SimpleItem(new SimpleProduct("Product 2", 11.99), 3.5));
-        invoice.addItem(new SimpleItem(new SimpleProduct("Product 3",  6.49), 5.0));
+        invoice.addItem(new SimpleItem(new SimpleProduct("Product 3", 6.49), 5.0));
         //Then
-        Assert.assertEquals(108.975, invoice.getValueToPay(), 0.001);
+        assertEquals(108.975, invoice.getValueToPay(), 0.001);
     }
+
 }
